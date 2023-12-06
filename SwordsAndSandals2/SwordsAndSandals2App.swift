eb43cbd96f697a753be4849344c4f2d1c312300f
Tyler Lawrence <tyler.lawrence15@gmail.com>
2023-12-05 19:52:56 -0500

    Initial Commit

---
+++ SwordsAndSandals2/SwordsAndSandals2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwordsAndSandals2App.swift
+//  SwordsAndSandals2
+//
+//  Created by Tyler Lawrence1 on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SwordsAndSandals2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
