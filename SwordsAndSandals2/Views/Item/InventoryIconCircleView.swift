deb49317c3e931f7db8d672916073e735c5931d9
Tyler Lawrence <tyler.lawrence15@gmail.com>
2024-03-14 08:59:24 -0400

    added item circle highlight aniimation

--- SwordsAndSandals2/Views/Item/InventoryIconCircleView.swift
+++ SwordsAndSandals2/Views/Item/InventoryIconCircleView.swift
@@ -8,17 +8,42 @@
 import SwiftUI
 
 struct InventoryIconCircleView: View {
-    let frameSize: CGFloat = 70
-    var color: Color = .blue
+    let size: CGFloat = 70
+    let scale: Double = 1.3
+    var color: Color = .gray
+    var isHighlighted: Bool = false
+    @State var degrees: Double = 360.0
+    
     var body: some View {
-        Circle()
-            .foregroundColor(color)
-            .opacity(0.2)
-            .frame(width: frameSize, height: frameSize)
+        ZStack{
+            if isHighlighted{
+                ZStack {
+                    Rectangle()
+                        .fill(AngularGradient(gradient: Gradient(colors: [.yellow, .white]), center: .center, angle: .degrees(degrees)))
+                        .brightness(0.2)
+                        .saturation(0.9)
+                        .blur(radius: 3)
+                }
+                .onAppear {
+                    withAnimation(Animation.linear(duration: 2).repeatForever(autoreverses: false)) {
+                        degrees = 0
+                    }
+                }
+                .clipShape(Circle())
+                .frame(width: size * scale, height: size * scale)
+            }
+            Circle()
+                .foregroundColor(color)
+                .frame(width: size, height: size)
+        }
+        
     }
 }
 
 #Preview {
-    InventoryIconCircleView()
+    VStack{
+        InventoryIconCircleView()
+        InventoryIconCircleView(isHighlighted: true)
+    }
 }
 
