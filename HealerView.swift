7896de674505334db5658ea1aead92429ddfa2fe
Tyler Lawrence <tyler.lawrence15@gmail.com>
2024-03-25 13:01:52 -0400

    player can now buy healing

---
+++ HealerView.swift
@@ -0,0 +1,35 @@
+//
+//  HealerView.swift
+//  SwordsAndSandals2
+//
+//  Created by Tyler Lawrence1 on 3/25/24.
+//
+
+import SwiftUI
+
+struct HealerView: View {
+    
+    @Environment(GameManager.self) var gameManager
+    @State var player: GameCharacter
+    
+    var body: some View {
+        VStack{
+            HStack{
+                HealButton(service: .five)
+                HealButton(service: .full)
+            }
+            .environment(player)
+            .padding(.bottom)
+            
+            Button("return to town"){
+                gameManager.gameState = .main
+            }
+            .buttonStyle(.borderedProminent)
+        }
+    }
+}
+
+#Preview {
+    HealerView(player: GameCharacter.sample)
+        .environment(GameManager.sample)
+}
